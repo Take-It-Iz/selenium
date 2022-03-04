96253450b02150d0e627e9339a92c14b6ed16da9
Ivan Zurilov <johnydevprofile@gmail.com>
2022-03-04 19:35:56 +0200

    Initial commit

---
+++ pages_navigation.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_driver_path = "D:\Installations\Software\chromedriver" 
+chrome_driver = webdriver.Chrome(chrome_driver_path)
+chrome_driver.maximize_window()
+chrome_driver.get("https://realpython.com/")
+
+
+try:
+    learn_dropdown = WebDriverWait(chrome_driver, 10).until(
+        EC.presence_of_element_located((By.ID, "navbarDropdownLibrary"))
+    )
+    learn_dropdown.click() # click on dropdown menu 
+
+    learning_paths = WebDriverWait(chrome_driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//a[@class='dropdown-item'][4]"))
+    )
+    learning_paths.click() # click on 'Learning Paths'
+    chrome_driver.back() # step back
+    chrome_driver.forward() 
+
+finally:
+    chrome_driver.quit()
